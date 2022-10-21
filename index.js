9dbefefa4ae2e98ab076eb2734775bf576c1711e
Marianne Garrido <64554585+Marianne-90@users.noreply.github.com>
2022-10-21 19:44:20 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+class Estudiante {
+  constructor(nombre){
+    this.nombre= nombre
+    this.asignaturas=['Javascript', 'HTML', 'CSS']
+  }
+  
+  obtenDatos(){
+    return {nombre: this.nombre, asignaturas: this.asignaturas}
+  };
+  
+};
+
+const nuevoEstudiante = new Estudiante("Juan")
+
+console.log(nuevoEstudiante.obtenDatos())
